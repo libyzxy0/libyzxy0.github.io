8f566306d7ef659db00f3a82328ec418c333f755
Jan Liby Dela Costa <107909653+libyzxy0@users.noreply.github.com>
2022-07-15 07:26:14 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,5 +36,26 @@ $(document).ready(function(){
   });
 
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 //Disable Context Menu On chrome
 window.oncontextmenu = function() { return false; }
