9385356a565612cdff8aeea349690b41e2c2727f
Jan Liby Dela Costa <107909653+libyzxy0@users.noreply.github.com>
2022-07-20 20:51:14 +0800

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,12 +1,4 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  * https://web.facebook.com/Br4inyFools/
-  */
-
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'janlibydelacosta@gmail.com';
 
