f5e9de6ae3d179c02dfcb296db8d8f7401eac811
Jan Liby Dela Costa <107909653+libyzxy0@users.noreply.github.com>
2022-07-28 16:09:59 +0800

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,22 +36,13 @@ $(document).ready(function(){
   });
 
 });
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
+//Modal
+var modal = document.getElementById("myModal_1");
+var btn = document.getElementById("myBtn_1");
+var span = document.getElementsByClassName("close")[0]; 
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
